b635b695488c1542d2c09c2489e8eeedb14a0aa0
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-03-03 21:54:30 -0800

    car goes vroom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("hi!")
+//Variables
+function moveCar () {
+  const car = document.getElementById('car');
+  
+  let position = 0;
+  const interval = setInterval(frame, 50);
+
+  function frame () {
+    if (position === 800) {
+      clearInterval(interval);
+    }
+    position += 5;
+    car.style.left = position + 'px';
+    console.log(car.style.left);
+    console.log(position)
+  }
+}
+
+
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  moveCar();
+};
